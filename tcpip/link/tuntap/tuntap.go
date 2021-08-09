769040cb813257f66b748bdc4c374d7d8c0b0ce6
impact-eintr <yixingwei4@gmail.com>
2021-08-09 20:54:57 +0800

    tcp 经典应用

--- tcpip/link/tuntap/tuntap.go
+++ tcpip/link/tuntap/tuntap.go
@@ -2,7 +2,10 @@ package tuntap
 
 import (
 	"errors"
+	"fmt"
+	"os/exec"
 	"syscall"
+	"unsafe"
 )
 
 const (
@@ -44,6 +47,38 @@ func NewNetDev(c *Config) (fd int, err error) {
 
 }
 
+// SetLinkUp 让系统启动该网卡
+func SetLinkUp(name string) (err error) {
+	out, cmdErr := exec.Command("ip", "link", "set", name, "up").CombinedOutput()
+	if cmdErr != nil {
+		err = fmt.Errorf("%v:%v", cmdErr, string(out))
+		return
+	}
+	return
+}
+
+// SetRoute 通过ip命令添加路由
+func SetRoute(name, cidr string) (err error) {
+	out, cmdErr := exec.Command("ip", "link", "set", name, "up").CombinedOutput()
+	if cmdErr != nil {
+		err = fmt.Errorf("%v:%v", cmdErr, string(out))
+		return
+	}
+	return
+
+}
+
+// SetRoute 通过ip命令添加IP地址
+func AddIP(name, ip string) (err error) {
+	out, cmdErr := exec.Command("ip", "link", "set", name, "up").CombinedOutput()
+	if cmdErr != nil {
+		err = fmt.Errorf("%v:%v", cmdErr, string(out))
+		return
+	}
+	return
+
+}
+
 func newTun(name string) (int, error) {
 	return open(name, syscall.IFF_TUN|syscall.IFF_NO_PI)
 
@@ -56,9 +91,27 @@ func newTAP(name string) (int, error) {
 
 // 先打开一个字符串设备，通过系统调用将虚拟网卡和字符串设备fd bind在一起
 func open(name string, flags uint16) (int, error) {
+	// 打开一个字符串设备 得到自负设备的文件描述符
 	fd, err := syscall.Open("/dev/net/tun", syscall.O_RDWR, 0)
 	if err != nil {
 		return -1, err
 	}
+
+	var ifr struct {
+		name  []byte
+		flags uint16
+		_     [22]byte
+	}
+
+	copy(ifr.name[:], name)
+	ifr.flags = flags
+	// 通过ioctl系统调用 将fd和虚拟网卡驱动绑定在一起
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd),
+		syscall.TUNSETIFF, uintptr(unsafe.Pointer(&ifr)))
+	if errno != 0 {
+		syscall.Close(fd)
+		return -1, errno
+	}
+	return fd, nil
 
 }
